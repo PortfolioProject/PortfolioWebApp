6fe5dfe9506421e12ded1d6baedfff30625700ae
Gaurav Ojha <gauravojha@gmail.com>
2018-07-01 20:56:45 -0400

    Adding docker-compose.yml

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_restful import Api
-from .resources.stocks import Stock
+from backend.resources.stocks import Stock
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
 api.add_resource(Stock, '/stock')
