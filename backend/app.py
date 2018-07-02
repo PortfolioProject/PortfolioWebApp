6abbb2b314cd519fa75f0185c31d34d3e6c1b173
Gaurav Ojha <gauravojha@gmail.com>
2018-07-01 21:13:39 -0400

    Fixing CORS

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from backend.resources.stocks import Stock
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
