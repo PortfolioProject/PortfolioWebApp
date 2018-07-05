613b97921bd18c5b51a0ea7db31b8ce9be6cf76d
Gaurav Ojha <gojha@onetrust.com>
2018-07-05 17:07:26 -0400

    Work done with Initial Data

--- portfolio/portfolioManager/admin.py
+++ portfolio/portfolioManager/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from portfolioManager.models import Stocks, UserPortfolio
 
 # Register your models here.
+admin.site.register(Stocks)
+admin.site.register(UserPortfolio)
