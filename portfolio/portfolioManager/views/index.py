a2537e4ee5854f0eb1fab51c4dfb40feac581c46
Gaurav Ojha <gauravojha@gmail.com>
2018-07-15 19:51:02 -0400

    #7 UI Form for adding stock to portfolio

--- portfolio/portfolioManager/views/index.py
+++ portfolio/portfolioManager/views/index.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 from django.views import View
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from portfolioManager.models import UserPortfolio
+from portfolioManager.models import UserPortfolio, Stocks
 from django.contrib.messages import get_messages
+from portfolioManager.forms import stock_forms
 
 class IndexView(LoginRequiredMixin, View):
     def get(self, request):
@@ -12,9 +13,8 @@ class IndexView(LoginRequiredMixin, View):
         error_message = None
         for message in storage:
             error_message = message
-
         user_portfolio = UserPortfolio.objects.filter(user_id=request.user.id)
         context = {'user_portfolio': user_portfolio,
-                   'error_message': error_message}
-
+                   'error_message': error_message,
+                   'add_stock_form': stock_forms.AddStockForm()}
         return render(request, 'portfolioManager/index.html', context)
